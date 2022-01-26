a83177b9369d7bf6ee06280081c41912b4aefcb3
Taiki Noda <taiki.nd.real@gmail.com>
2022-01-26 23:13:10 +0900

    ページの読み込み

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "io/ioutil"
+import (
+	"fmt"
+	"io/ioutil"
+)
 
 type Page struct {
 	Title string
@@ -12,6 +15,19 @@ func (p *Page) Save() error {
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
 func main() {
 	p1 := &Page{Title: "title", Body: []byte("this is a sample page")}
+	p1.Save() // title.txtで"this is a sample page"と書いてあるファイルを作成する
+
+	p2, _ := loadPage(p1.Title)
+	fmt.Println(string(p2.Body))
 }
